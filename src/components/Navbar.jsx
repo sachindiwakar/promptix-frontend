cdc38e62011c6f1e77ced2ff2203a376028f31dc
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-02 23:23:41 +0530

    Initial navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router";
+import { ArrowRight } from "lucide-react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-40 sm:w-52 md:w-64 lg:w-72"
+        onClick={() => navigate("/")}
+      />
+      <button className="flex items-center gap-2 rounded-full text-sm bg-primary text-white px-10 py-2.5 cursor-pointer">
+        Get Started <ArrowRight className="w-4 h-4" />
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
