d8cbf129607b0dbcfd92f25e8bf5b5e120e0dc20
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-02 23:39:21 +0530

    Add Clerk Auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 );
