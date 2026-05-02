86182ed75183c92af68a5783a548790dbfebc6df
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-03 01:39:37 +0530

    Complete hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AiTools from "../components/AiTools";
+import Testimonial from "../components/Testimonial";
+import Plan from "../components/Plan";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
     </>
   );
 };
