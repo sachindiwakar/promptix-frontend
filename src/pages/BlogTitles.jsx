b31bc791298c6b6cc6f23c834dd0d45af206975a
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-08 02:21:47 +0530

    connect Blog Tiles page to its API.

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import { Hash, Sparkles } from "lucide-react";
+import axios from "axios";
+import { useAuth } from "@clerk/react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const BlogTitles = () => {
   const blogCategories = [
@@ -7,7 +13,7 @@ const BlogTitles = () => {
     "Technology",
     "Business",
     "Health",
-    "Liestyle",
+    "Lifestyle",
     "Education",
     "Travel",
     "Food",
@@ -16,8 +22,34 @@ const BlogTitles = () => {
   const [selectedCategory, setSelectedCategory] = useState("General");
   const [input, setInput] = useState("");
 
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  const { getToken } = useAuth();
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      const prompt = `Generate 5 SEO-friendly and catchy blog titles for the keyword ${input}. Blog category/type: ${selectedCategory}.Return only the titles in a numbered list.`;
+      const { data } = await axios.post(
+        "/api/ai/generate-blog-title",
+        { prompt },
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        },
+      );
+      if (data.content) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   };
 
   return (
@@ -53,8 +85,15 @@ const BlogTitles = () => {
           ))}
         </div>
         <br />
-        <button className="w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
-          <Hash className="w-5" />
+        <button
+          disabled={loading}
+          className="w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Hash className="w-5" />
+          )}
           Generate Title
         </button>
       </form>
@@ -68,12 +107,20 @@ const BlogTitles = () => {
           <h1 className="text-xl font-semibold">Generated Titles</h1>
         </div>
 
-        <div className="flex-1 flex  justify-center items-center">
-          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Hash className="w-9 h-9" />
-            <p>Enter a topic and click "Generate Title" to get started.</p>
+        {!content ? (
+          <div className="flex-1 flex  justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <Hash className="w-9 h-9" />
+              <p>Enter a topic and click "Generate Title" to get started.</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
